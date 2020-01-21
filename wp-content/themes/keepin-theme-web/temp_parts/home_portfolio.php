aabea1f81dd6cacad23cbbdcaac7b59c3565138e
m3stiz0 <kmestizo@gmail.com>
2020-01-21 12:58:23 -0400

    home_portafolio

--- wp-content/themes/keepin-theme-web/temp_parts/home_portfolio.php
+++ wp-content/themes/keepin-theme-web/temp_parts/home_portfolio.php
@@ -9,18 +9,19 @@ $nuevo_arreglo = new WP_Query(array(
 ?>
 <div class="row container-fluid col-xs-12 d-lg-none p-0 m-0">
     <?php if ($nuevo_arreglo->have_posts()) : 
-        while ($nuevo_arreglo->have_posts()) :
-			$nuevo_arreglo->the_post();?>
+        while ($nuevo_arreglo->have_posts()) : $nuevo_arreglo->the_post();?>
                 <div class="portfolio col-12 pt-3 pl-2 m-0">
                     <div>
                         <a class="titulo_portfolio text-white p-0 m-0" href="<?php the_permalink(); ?>">
                             <?php the_title();?>
                         </a>
                     </div>
-                    <div class=" ">
-                        <a class="col-8 post_portfolio text-white p-0 m-0" href="<?php the_permalink(); ?>">
-                            <?php the_content();?>
-                            <img src="<?php echo get_template_directory_uri() . '/img/btn-vermas_trans.png'; ?>">
+                    <div class="row d-flex-row ">
+                        <a class="post_portfolio text-white p-0 m-0" href="<?php the_permalink(); ?>">
+                             <div class="col-10 float-left"><?php the_content();?></div>
+                             <div class="col-2 float-right">
+                                 <img src="<?php echo get_template_directory_uri() . '/img/btn-vermas_trans.png'; ?>">
+                            </div>
                         </a>
                     </div>
                 </div>
@@ -29,6 +30,7 @@ $nuevo_arreglo = new WP_Query(array(
                             col-12 p-0 m-0 regular shadow" >
                     <?php the_post_thumbnail('medium'); ?>
                 </div>
-        <?php endwhile;?>        
+        <?php endwhile; wp_reset_postdata();?>   
+
     <?php endif;?>
 </div>
