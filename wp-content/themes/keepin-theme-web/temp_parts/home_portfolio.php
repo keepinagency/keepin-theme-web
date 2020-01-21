8fee309ac9433e0bee46569d33b915a8e1c97f5c
m3stiz0 <kmestizo@gmail.com>
2020-01-21 11:35:47 -0400

    home_start

--- wp-content/themes/keepin-theme-web/temp_parts/home_portfolio.php
+++ wp-content/themes/keepin-theme-web/temp_parts/home_portfolio.php
@@ -11,14 +11,20 @@ $nuevo_arreglo = new WP_Query(array(
     <?php if ($nuevo_arreglo->have_posts()) : 
         while ($nuevo_arreglo->have_posts()) :
 			$nuevo_arreglo->the_post();?>
-                <div class="portfolio p-0 m-0 w-100">
-                    <a class="titulo_portfolio text-white" href="<?php the_permalink(); ?>">
-                        <h3><?php the_title();?></h3>
-                    </a>
-                    <a class="post_portfolio text-white" href="<?php the_permalink(); ?>">
-                        <h5><?php the_content();?></h4>
-                    </a>
+                <div class="portfolio col-12 pt-3 pl-2 m-0">
+                    <div>
+                        <a class="titulo_portfolio text-white p-0 m-0" href="<?php the_permalink(); ?>">
+                            <?php the_title();?>
+                        </a>
+                    </div>
+                    <div class=" ">
+                        <a class="col-8 post_portfolio text-white p-0 m-0" href="<?php the_permalink(); ?>">
+                            <?php the_content();?>
+                            <img src="<?php echo get_template_directory_uri() . '/img/btn-vermas_trans.png'; ?>">
+                        </a>
+                    </div>
                 </div>
+
                 <div class="d-flex align-items-center justify-content-center
                             col-12 p-0 m-0 regular shadow" >
                     <?php the_post_thumbnail('medium'); ?>
--- wp-content/themes/keepin-theme-web/temp_parts/home_start.php
+++ wp-content/themes/keepin-theme-web/temp_parts/home_start.php
@@ -1,13 +1,22 @@
 <!--Luego de header-->
-<div class="row col-xs-12 d-lg-none p-0 m-0 border">
-    <div class="col-4 mt-2"> hola </div>
-    <div class="col-4 mt-2">hey</div>
-    <div class="col-12 mt-2">We build solutions </div>
-    
-    <!--div class="row"></div-->
-    <div class="col-12 mb-2">...on web</div>
-    <div class="col-12"> 
-        <button type="button" class="btn-home btn btn-default">Get Started!</button> 
+<div class="row col-xs-12 d-lg-none d-flex flex-row justify-content-center">
+    <div class="col-6 pr-3 m-0">
+        <img class="float-right" src="<?php echo get_template_directory_uri() . '/img/multi_platform.png'; ?>">
     </div>
-    <div class="col-12 mt-3" style="background-color:#413a4d; height:10px;"> </div>
+
+    <div class="col-6 pl-3 m-0">
+        <img src="<?php echo get_template_directory_uri() . '/img/easy_integration.png'; ?>">
+    </div>
+
+    <div class="col-12 text-center txt-home-start">We build solutions</div>
+    <div class="col-12 text-center txt-home-start">...on web.</div>
+
+    <div class=" text-center btn-home-start shadow-sm rounded"> 
+        <button type="button" class="btn btn-default btn-txt-home-start text-white">Get Started!</button> 
+    </div>
+    <div class="col-12 mt-4 mb-5" style="background-color:#413a4d; height:10px;"></div>
 </div>
+
+
+
+
