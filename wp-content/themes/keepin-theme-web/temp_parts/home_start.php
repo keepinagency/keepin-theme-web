9d87e4c8daa7787fdcce436cf4271576ad1dc952
Javier Ramos <javerne@gmail.com>
2020-01-22 14:06:48 -0400

    Ordenamiento de items en Home Start y css.

--- wp-content/themes/keepin-theme-web/temp_parts/home_start.php
+++ wp-content/themes/keepin-theme-web/temp_parts/home_start.php
@@ -1,20 +1,18 @@
 <!--Luego de header-->
-<div class="row col-xs-12 d-lg-none d-flex flex-row justify-content-center">
-    <div class="col-6 pr-3 m-0">
-        <img class="float-right" src="<?php echo get_template_directory_uri() . '/img/multi_platform.png'; ?>">
-    </div>
-
-    <div class="col-6 pl-3 m-0">
+<div class="row col-12 d-lg-none d-flex flex-row justify-content-center p-0 pt-2 m-0 mb-2">
+    
+    <div class="col-12 text-center pt-3">
+        <img src="<?php echo get_template_directory_uri() . '/img/multi_platform.png'; ?>">
         <img src="<?php echo get_template_directory_uri() . '/img/easy_integration.png'; ?>">
     </div>
 
-    <div class="col-12 text-center txt-home-start">We build solutions</div>
-    <div class="col-12 text-center txt-home-start">...on web.</div>
-
-    <div class=" text-center btn-home-start shadow-sm rounded"> 
-        <button type="button" class="btn btn-default btn-txt-home-start text-white">Get Started!</button> 
+    <div class="col-12 text-center txt-home-start pt-2">
+        We build solutions<br>...on web.
     </div>
-    <div class="col-12 mt-4 mb-5" style="background-color:#413a4d; height:10px;"></div>
+
+    <button type="button" class="btn btn-home-start align-middle">Get Started!</button> 
+
+    <div class="col-12 mt-4" style="background-color:#413a4d; height:10px;">&nbsp;</div>
 </div>
 
 
