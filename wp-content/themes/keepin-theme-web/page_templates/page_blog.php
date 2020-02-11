dd2146bbcb1c8ac233f4aa2b2ac670ec7907ec8b
m3stiz0 <kmestizo@gmail.com>
2020-02-11 10:58:40 -0400

    correccion blog and team

--- wp-content/themes/keepin-theme-web/page_templates/page_blog.php
+++ wp-content/themes/keepin-theme-web/page_templates/page_blog.php
@@ -47,31 +47,33 @@ $nuevo_arreglo = new WP_Query(array(
                             pt-lg-0 pr-lg-2 pl-lg-2 col-lg-4">
                     <div class="d-none d-sm-block col-lg-12 p-lg-0 cont-img pt-lg-3 pb-lg-1"
                         style="background-image: url('<?php echo get_the_post_thumbnail_url('');?>');">
-                    &nbsp;
+                        &nbsp;
                         <!--a href="<?php the_permalink(); ?>">
                             <?php //the_post_thumbnail('blog') ;?>
                         </a-->
                     </div>        
                     <div class="row contblog_post col-12 m-0 p-0">
                         <div class="col-10 col-lg-8 flex-lg-row m-0 p-0 cont_tit_blog">
-                            <a class="text-white p-0 m-0 text-decoration-none" href="<?php the_permalink(); ?>">
-                                <div class="p-0 m-0 titleblog_post pl-lg-2">
+                            <a class="text-white p-0 m-0" href="<?php the_permalink(); ?>">
+                                <div class="p-2 m-0 titleblog_post pl-lg-2 text-white">
                                     <?php the_title();?>
                                 </div>
-                                <div class="p-0 m-0 postblog_post text-lg-justify pl-lg-2">
+                                <div class="p-2 m-0 postblog_post text-lg-justify pl-lg-2 text-white">
                                     <?php the_excerpt();?>
                                 </div>
                             </a>
                         </div>
-                        <div class="col-2 p-0 m-0 col-lg-4 d-flex align-items-center 
+                        <div class="col-2 p-0 m-0 col-lg-4 d-flex align-items-center  
                         align-items-lg-center justify-content-lg-center p-lg-0 m-lg-0">
-                            <a class="p-0 m-0 text-white " href="<?php the_permalink(); ?>">
+                            <a class="p-0 m-0 text-white" href="<?php the_permalink(); ?>">
                                 <img src="<?php echo get_template_directory_uri() . '/img/btn-vermas_trans.png'; ?>">
                             </a>
                         </div>
                     </div>
-                    <div class="d-flex align-items-center justify-content-center col-12 pt-2 pb-4 m-0 d-lg-none">
-                        <?php the_post_thumbnail('medium'); ?>
+                    <div class="d-flex align-items-center justify-content-center col-12 pt-2 pb-4 m-0 d-lg-none cont-img"
+                    style="background-image: url('<?php echo get_the_post_thumbnail_url('');?>');">
+                        &nbsp;
+                        <?php //the_post_thumbnail('medium'); ?>
                     </div>
 
                 </div>
